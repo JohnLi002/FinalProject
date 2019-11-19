b0e534e5346cc8e92be9bac52944025b760666a8
John Li <lij19@wit.edu>
2019-11-19 13:24:22 -0500

    Was working on having multiple people in the server at once

--- Server.py
+++ Server.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import socket
+
+def messageAll(connected, msg):
+    i = len(connected)
+    response = str(i) + " people have been connected"
+    for x in connected:
+        x.send(response.encode())
+
+def check(connected, check):
+    for x in connected:
+        if x is check:
+            return True
+    print("something")
+    return False
+
 def server_program():
     # get the hostname
     host = socket.gethostname()
@@ -13,27 +27,44 @@ def server_program():
     # look closely. The bind() function takes tuple as argument
     server_socket.bind((host, port))  # bind host address and port together
     # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
+    amount = 1
+    server_socket.listen(amount)
     addresses = []
+    names = []
     i = 0
     
-    while True:
-        conn, address = server_socket.accept()  # accept new connection
+    while(len(addresses) != amount):
+        conn, address = server_socket.accept()
         print("Connection from: " + str(address))
-        if(i < 2):
-            addresses.append(conn)
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = addresses[i%2].recv(1024).decode()
+        username = conn.recv(1024).decode()
+        print(username)
+        names.append(username)
+        addresses.append(conn)
+    
+    messageAll(addresses, " people have been connected")
+    
+    while amount > 0:
+        data = addresses[i%amount].recv(1024).decode()
         if not data:
             # if data is not received break
             break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        addresses[i%2].send(data.encode())  # send data to the client
-        i += 1        
-
+        if data == 'bye':
+            globalMsg = "[" + names[i%amount] + "] has left the server"
+            del(names[i%amount])
+            del(addresses[i%amount])
+            amount -= 1
+            messageAll(addresses, globalMsg)
+        else:
+            print("from [" + names[i%amount] + "]: " + str(data))
+            data = input(' -> ')
+            addresses[i%amount].send(data.encode())  # send data to the client
+            i += 1
 
     conn.close()  # close the connection
 
 if __name__ == '__main__':
     server_program()
+
+
+
+    
