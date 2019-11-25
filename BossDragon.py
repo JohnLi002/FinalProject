a9796afbc964c285ff588db2e5f85c91a894f181
Holyglade <39798943+Holyglade@users.noreply.github.com>
2019-11-25 15:34:23 -0500

    Replaced Glare with Bite

--- BossDragon.py
+++ BossDragon.py
@@ -8,7 +8,12 @@ import random
 
 
 class BossDragon:
+<<<<<<< Updated upstream
     actions = ["Tail Swipe", "Dragon Breath", "Glare"]
+=======
+    actions = ["Tail Swipe", "Dragon Breath", "Bite"]
+    health = 0
+>>>>>>> Stashed changes
     
     def __init__(self, health):
         self.health = health
